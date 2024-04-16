eba4713e0f791a208b0ccb26be732126a65fe3d7
naelafauzul <naelafauzulm@gmail.com>
2024-04-16 15:16:10 +0700

    Initial Commit

---
+++ TriangleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriangleApp
+//
+//  Created by Naela Fauzul Muna on 16/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
